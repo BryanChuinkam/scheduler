ecc3f59bbf2c08efd681bf0823a6e9607a697b62
bryanCN <bryan.chuinkam@gmail.com>
2023-06-21 10:05:24 -0400

    added state managemnt for scheduling

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,21 @@
+import { useState } from "react";
+
+export default function useVisualMode(initial) {
+  const [history, setHistory] = useState([initial]);
+
+  function transition(newMode, replace = false) {
+    if (replace) {
+      setHistory(prev => [...prev.slice(0, prev.length - 1), newMode]);
+
+    } else {
+
+      setHistory(prev => [...prev, newMode]);
+    }
+  }
+
+  function back() {
+    history.length > 1 ? setHistory(prev => [...prev.slice(0, prev.length - 1)]) : setHistory(prev => [...prev]);
+  }
+
+  return { mode: history[history.length - 1], transition, back };
+}
